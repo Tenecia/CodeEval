5e38412c7c0f751975615ab7f26541eea5961727
Tenecia Guinea <jasminegheith@gmail.com>
2015-07-21 23:50:05 -0400

    submission

---
+++ SumofDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalSumofDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            string i = "12345";
+            int s = i.Sum(c => Convert.ToInt32(c - '0'));
+            Console.WriteLine(s);
+            Console.ReadLine();          
+
+
+        }
+    }
+}
