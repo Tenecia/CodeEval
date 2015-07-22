a228f4db4cafaf42a81870a71ff65aa2f4a30725
Tenecia Guinea <jasminegheith@gmail.com>
2015-07-21 21:15:47 -0400

    submission

---
+++ EvenNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+                        
+            for (i = 0; i <= 5000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    System.Console.WriteLine("0");
+                }
+
+                if (i % 2 != 0)
+                {
+                    System.Console.WriteLine("1");
+                }
+
+            }
+            Console.ReadLine();
+        }
+        
+
+    }
+}
