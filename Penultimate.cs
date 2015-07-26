5044e985fd3256d0c1692d81b9614e3311729c5f
Tenecia Guinea <jasminegheith@gmail.com>
2015-07-26 13:29:30 -0400

    penultamate

---
+++ Penultimate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEvalPenUltamateWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string mystring = "this is right";
+            //string s = mystring.split('');
+            //Console.WriteLine(s[s.Length-2]);
+         
+            //Console.Writeline();
+
+            String data="this will work too" ;
+            var dataRows = data.Split(' ');
+            System.Console.WriteLine(dataRows[dataRows.Length-2]);
+            System.Console.ReadLine();
+
+        }
+
+           
+    }
+
+
+}
